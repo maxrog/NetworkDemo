3bf86a0b0f55a0f3164495243926bf963781570f
Max Rogers <marfodub@gmail.com>
2017-11-25 16:52:10 -0600

    Initial Project

---
+++ NetworkDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  NetworkDemo
+//
+//  Created by Max Rogers on 11/25/17.
+//  Copyright © 2017 Max Rogers. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
